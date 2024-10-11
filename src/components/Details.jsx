369a7f624803c2b1b4f70fe4305ef90ae22abdda
DILEEP-KUMAR-7 <dileepkumarnaidu2345@gmail.com>
2024-10-11 14:44:33 +0530

    Project Portfolio

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -27,7 +27,7 @@ function Details() {
 
             {/* <button className="border border-2 border-black px-2 py-1 m-2 rounded">Download Resume</button> */}
             <div className='flex justify-end m-3 gap-2'>
-            <a className='' href='../assets/MASTHAN JAVID VALI SHAIK.pdf' download={true}>
+            <a className='' href='../assets/BORAGADDADILEEP KUMAR_portfolio.pdf' download={true}>
             <button type="button"
                 data-twe-ripple-init
                 data-twe-ripple-color="black"
@@ -43,21 +43,21 @@ function Details() {
             {/* profile summary section */}
             <div className='m-3 p-3 rounded  opacity-95'>
                 <h1 className="Profile my-2 font-bold text-xl">Profile</h1>
-                <p className='m-3'>A highly motivated and dedicated fresher with a strong educational background and practical experience in web development, Salesforce, and machine learning. Committed to leveraging academic achievements, technical skills, and enthusiasm to contribute effectively in a professional environment.Seeking an opportunity to apply and further develop skills in a dynamic work setting.</p>
+                <p className='m-3'>A highly motivated and dedicated recent graduate with a solid foundation in web development, Salesforce, and Artificial Intelligence - Machine Learning. Equipped with both academic knowledge and hands-on experience, I am committed to delivering results and continuously enhancing my technical skills. Eager to contribute to a dynamic work environment, I am seeking an opportunity to apply my expertise while furthering my professional growth .</p>
             </div>
             {/* internships section */}
             <div className='m-3 p-3 rounded  opacity-95'>
                 <h1 className="Experience my-2 font-bold text-xl ">Experience</h1>
 
-                <Internship name={'Web Development with WordPress Internship'} duration={'Jan 2024 - Apr 2024'} organization={"SmartInternz"}
-                    items={['Improved website functionality by 90% through plugin integration, database management, and theme customization.', 'Investigated website deployment and hosting, guaranteed up to 75% mitigation of security threats.']}
+                <Internship name={'DATA ANALYTICS with TABLEAU  Internship'} duration={'Jan 2024 - Apr 2024'} organization={"SmartInternz"}
+                    items={[' cleaning and analyzing data to uncover insights, using Tableau to create interactive visualizations and dashboards that support business decision-making.', 'applying analytical skills to align data with business goals, while effectively communicating findings and collaborating with teams to solve real-world problems using data.']}
                 />
 
-                <Internship name={'Front-end Web Development Internship'} duration={'May 2023 - Aug 2023'} organization={'SmartInternz'}
-                    items={['Executed 85% of design concepts, Attained 90% cross-browser compatibility and responsive design.', 'Coordinated effectively with a development team of 4 members.', 'Employed professional skills such as HTML (86%), CSS (10%), and JavaScript (4%).']}
+                <Internship name={'SOFTWARE TESTING AUTOMATION Internship'} duration={'May 2023 - Aug 2023'} organization={'SmartInternz'}
+                    items={[' focusing on creating and executing automated test scripts using tools like Selenium and programming languages such as Java or Python to enhance testing efficiency and accuracy..', 'working closely with development teams, using tools like JIRA and Git to track bugs, report defects, and ensure continuous integration, while also gaining experience with test automation frameworks and performance testing tools.']}
                 />
 
-                <Internship name={'Salesforce Developer Internship'} duration={'Aug 2022 - Oct 2022'} organization={'SmartInternz'}
+                <Internship name={'Salesforce Developer Internship'} duration={'Sep 2022 - Nov 2022'} organization={'SmartInternz'}
                     items={['Enhanced knowledge by 65% through engagement with the Salesforce community.', 'Accomplished 7 modules and 2 super-badge modules, acquiring practical skills in Apex programming, Relationship & Process Automation, and Lightning components.']}
                 />
 
@@ -73,21 +73,22 @@ function Details() {
                             <IoCalendar /><h4 className='font-bold text-md'>2020 - 2024</h4>
                         </span>
                     </span>
-                    <h1 className=''> Computer Science and Engineering</h1>
+                    <h1 className=''> Computer Science and Engineering (DATA SCIENCE)</h1>
                     <span className='flex items-center justify-between'>
-                        <h6 className='text-sm'>KALLAM HARANADHAREDDY INSTITUTE OF TECHNOLOGY</h6>
+                        <h6 className='text-sm'>CHEBROLU ENGINEERING COLLEGE </h6>
                         <span className='flex justify-center items-center gap-3'>
-                            <h4 className=' text-sm'>7.74 CGPA</h4>
+                            <h4 className=' text-sm'>7.5 CGPA</h4>
                         </span>
                     </span>
                 </div>
             </div>
 
             {/* Skills section */}
+
             <div className="m-3 p-3 rounded ">
                 <h1 className="Skills my-2 font-bold text-xl ">Skills</h1>
                 <div className="p-3 my-1">
-                    <Button skill={'c programming'} />
+            
                     <Button skill={'python programming'} />
                     <Button skill={'Html'} />
                     <Button skill={'css'} />
@@ -96,16 +97,16 @@ function Details() {
                     <Button skill={'tailwind-CSS'} />
                     <Button skill={'ECMAScript 6+'} />
                     <Button skill={'wordpress'} />
-                    <Button skill={'react js'} />
+                    <Button skill={'react js'} /> 
                     <Button skill={'mongodb'} />
                     <Button skill={'mysql'} />
                     <Button skill={'git'} />
                     <Button skill={'github'} />
                     <Button skill={'salesforce development'} />
                     <Button skill={'machine learning'} />
-                    <Button skill={'Microsoft Word'} />
-                    <Button skill={'Microsoft Excel'} />
-                    <Button skill={'Microsoft PowerPoint'} />
+                    <Button skill={'data science'} />
+                    <Button skill={'cloud'}/>
+
                 </div>
 
             </div>
@@ -115,27 +116,19 @@ function Details() {
                 <h1 className="Certifications my-2 font-bold text-xl">Certifications</h1>
                 <div className=' grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-1'>
 
-                <Certificate name={'DBMS Tutorial Module'} duration={'Jun 2024'} organization={'Scaler'} skills={'Python'} url={'https://moonshot.scaler.com/s/sl/agzLEAZdJg?_gl=1*12cmuc4*_gcl_au*NDY5MDgyMDU1LjE3MTQzNjY0NTY.*FPAU*NDY5MDgyMDU1LjE3MTQzNjY0NTY.*_ga*MTQ4MDQ3NDYyMS4xNzE0MzY2NDU2*_ga_53S71ZZG1X*MTcxOTQ2ODc4NS44Ny4xLjE3MTk0Njg4MTQuMC4wLjIwMjEzNjU3NDU.'} />
+                <Certificate name={'DATA SCIENCE for BEGINNERS'} duration={'APRIL 2023'} organization={'future skills prime'} skills={'Python'} url={'https://www.linkedin.com/posts/dileep-kumar-boragadda-43293822a_activity-7227009522375016448-6HLo?utm_source=share&utm_medium=member_desktop'} />
 
-                <Certificate name={'Python Course for Beginners: Mastering the Essentials '} duration={'Jun 2024'} organization={'Scaler'} skills={'Python'} url={'https://moonshot.scaler.com/s/sl/dCqrwpXYnH?_gl=1*1s6gkd4*_gcl_au*NDY5MDgyMDU1LjE3MTQzNjY0NTY.*FPAU*NDY5MDgyMDU1LjE3MTQzNjY0NTY.*_ga*MTQ4MDQ3NDYyMS4xNzE0MzY2NDU2*_ga_53S71ZZG1X*MTcxOTQ2ODc4NS44Ny4xLjE3MTk0Njg5ODIuMC4wLjIwMjEzNjU3NDU.'} />
+                <Certificate name={'SOFTWARE TESTING AUTOMATION'} duration={'AUG 2023'} organization={'SmartInternz'} skills={'Automation Testing'} url={'https://www.linkedin.com/posts/dileep-kumar-boragadda-43293822a_virtual-internship-on-software-testing-automation-activity-7226849531269627905-hqTT?utm_source=share&utm_medium=member_desktop'} />
 
-                <Certificate name={'C Tutorial'} duration={'May 2024'} organization={'Scaler'} skills={'Python'} url={'https://moonshot.scaler.com/s/sl/kImO5BgPLr?_gl=1*pfbigp*_gcl_au*NDY5MDgyMDU1LjE3MTQzNjY0NTY.*FPAU*NDY5MDgyMDU1LjE3MTQzNjY0NTY.*_ga*MTQ4MDQ3NDYyMS4xNzE0MzY2NDU2*_ga_53S71ZZG1X*MTcxOTQ2ODc4NS44Ny4xLjE3MTk0Njg5ODIuMC4wLjIwMjEzNjU3NDU.'} />
+                <Certificate name={'DATA ANALYTICS with TABLEAU'} duration={'APRIL 2024'} organization={'SmartInternz'} skills={'Python'} url={'https://www.linkedin.com/posts/dileep-kumar-boragadda-43293822a_activity-7227005512066678784-YKZA?utm_source=share&utm_medium=member_desktop'} />
 
-                <Certificate name={'Web Development With WordPress'} duration={'Apr 2024'} organization={'SmartInternz'} skills={'Web Development, WordPress'} url={'https://www.linkedin.com/in/masthan-javid-vali-shaik-3a15ba248/details/certifications/1716268524350/single-media-viewer/?type=DOCUMENT&profileId=ACoAAD1QPKkB1wLOp9qRGyNuNrkPCh-ekN97U3A'} />
+                <Certificate name={'Web Development With WordPress'} duration={'october 2022'} organization={'Ganga Apps'} skills={'Web Development, WordPress'} url={'https://www.linkedin.com/posts/dileep-kumar-boragadda-43293822a_activity-7238786428984250368-Kzrk?utm_source=share&utm_medium=member_desktop'} />
 
-                <Certificate name={'React.js Essential Training'} duration={'Sep 2023'} organization={'LinkedIn'} skills={'ReactJS'} url={'https://www.linkedin.com/learning/certificates/a61c9c7d63c632dda9ddc64441dc1f86f6dcbb7145fbaf05f187cee234ea39c6'} />
+                <Certificate name={'ETHICAL HACKING and CYBER SECURITY'} duration={'Mar 2022'} organization={'LYFAUX'} skills={'HACKING (basic)'} url={'https://www.linkedin.com/posts/dileep-kumar-boragadda-43293822a_activity-7230475737370087425-WCEZ?utm_source=share&utm_medium=member_desktop'} />
 
-                <Certificate name={'Learning ECMAScript 6+ (ES6+)'} duration={'Sep 2023'} organization={'LinkedIn'} skills={'ECMAScript 6+'} url={'https://www.linkedin.com/learning/certificates/c3fc20503dbc913cbc3ee801dd3f7912bc53881a59def3a5b51ffe23071b2684'} />
 
-                <Certificate name={'SmartInternz Long Term Virtual Internship on Front-End Development'} duration={'Sep 2023'} organization={'SmartInternz'} skills={'Front-End Webdevelopment (includs HTML, CSS, JavaScript and Boostrap)'} url={'https://apsche.smartinternz.com/certificate/student/03fcd68e5673f08be96d2b6bb5be8261'} />
+                <Certificate name={'Salesforce Developer Virtual Internship'} duration={'Oct 2022'} organization={'SmartInternz'} skills={'Salesforce Developement'} url={'https://www.linkedin.com/posts/dileep-kumar-boragadda-43293822a_activity-7227005703498907648-zwTX?utm_source=share&utm_medium=member_desktop'} />
 
-                <Certificate name={'HTML Essential Training'} duration={'Sep 2023'} organization={'LinkedIn'} skills={'HTML'} url={'https://www.linkedin.com/learning/certificates/9b18388cc7162b67975c768ad878e10b10374f2b480ed40aa7823ed242468dc9'} />
-
-                <Certificate name={'JavaScript Essential Training'} duration={'Aug 2023'} organization={'LinkedIn'} skills={'JavaScript'} url={'https://www.linkedin.com/learning/certificates/c0ba01404e2a974edaf99b82c33c35f423936f771bdbecd6a35aea492fffb843'} />
-
-                <Certificate name={'Salesforce Developer Virtual Internship'} duration={'Sep 2022'} organization={'SmartInternz'} skills={'Salesforce Developement'} url={'https://smartinternz.com/internships/salesforce_certificates/6d4e0869a271896a65f53e281d4ba846'} />
-
-                <Certificate name={'MongoDB Certificate'} duration={'Aug 2022'} organization={'CodeTantra'} skills={'MongoDB'} url={'CT0892212KHITCSEMDB'} />
                 </div>
             </div>
 
@@ -143,11 +136,7 @@ function Details() {
             <div className='m-3 p-3 rounded '>
                 <h1 className="Projects my-2 font-bold text-xl ">Projects</h1>
 
-                <Project name={'Bone Tumor Identification Using X-Ray Images with Machine Learning'} duration={'Jan 2024 - Apr 2024'} items={['Researched and analyzed advancements in bone tumor identification via X-ray imaging techniques, including AI algorithms and 3D imaging technologies; findings directly contributed to improving diagnostic accuracy by 20%.','Developed and deployed a Convolution Neural Network (CNN) model in a bone tumor detection project, trained algorithm using diverse bone images to achieve 90%+ accuracy in tumor prediction.','Engineered a comprehensive medical imaging system incorporating a fusion of traditional image processing techniques and cutting-edge deep learning algorithms; achieved a 40% increase in bone tumor detection accuracy and reduced false positives by 25%.']} skills={' Machine Learning, Python and Image Processing'} url={'https://github.com/javid4962/Bone_Tumor_Prediction'} />
-
-                <Project name={'Online Learning Platform'} duration={'Oct 2023 - Dec 2023'} items={['Created and deployed an Online Learning Platform which consists of features like User Login, New User Registration, Course selection, Cart Courses and their prices.','Engaged advanced Technologies to achieve more than 85% of cross-browser compatibility and Responsiveness.','Added some new features like AI course suggestion bot to make user Interaction.']} skills={'React.js & Tailwind CSS'} url={'https://github.com/javid4962/LEARNFLOW-Online-Learning-Platform'} />
-
-               
+                <Project name={'CryptCloud Secure and Expressive Data Access Control for Cloud Storage'} duration={'Feb 2024 - May 2024'} items={['CryptCloud+ aims to provide a secure and user-friendly system for data access control in cloud storage environments. It offers strong encryption and mitigates the risk of unauthorized access by introducing traceability mechanisms.']} skills={' java, java script, mysql, neatbeans'} url={'https://github.com/DILEEP-KUMAR-7/Crypt-Cloud-Secure-and-Expressive'} />
 
             </div>
         </div>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,15 +4,11 @@ import { IoMail } from "react-icons/io5";
 import { FaPhoneAlt } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { TfiLinkedin } from "react-icons/tfi";
-import { SiLeetcode } from "react-icons/si";
 import { FaDownload } from "react-icons/fa6";
-import { FaPenToSquare } from "react-icons/fa6";
-
-import Button from './Button';
 
 
 function Profile() {
-  const[image,setImage]=useState('./img2.png');
+  const[image,setImage]=useState('./bunty.jpg');
 
   useEffect(() => {
     const storedImage = localStorage.getItem('profileImage');
@@ -44,21 +40,21 @@ function Profile() {
 
         <input type="file" onChange={handleImageChange} accept="image/*" id="fileInput" className="d-none invisible" />
 
-        <span className="name text-2xl text-center font-bold p-4">Masthan Javid Vali Shaik</span> {/* Name of the Candidate */}
+        <span className="name text-2xl text-center font-bold p-4">BORAGADDA DILEEP KUMAR</span> {/* Name of the Candidate */}
 
         <span className='designation text-lg font-medium text-center p-2'>Web Developer</span>{/* Designation of the Candidate */}
 
-        <div className="contact py-4"> {/* style  bg-black text-white px-3 rounded-lg */}
-          <span className="flex items-center justify-items-center gap-2 text-lg p-1 m-0"><IoMail className="text-yellow-500" /><a className='email text-sm  text-center' href='mailto:shaikmasthanjavidvali@gmail.com'>shaikmasthanjavidvali@gmail.com</a></span> {/* email */}
-          <span className="flex items-center justify-items-center gap-2 text-lg p-1 m-0"><FaPhoneAlt className="text-blue-500" /><a className='phone text-sm  text-center' href='+91 9908797474'>+91 9908797474</a></span>{/* phone number */}
+        <div className="contact py-4"> 
+          <span className="flex items-center justify-items-center gap-2 text-lg p-1 m-0"><IoMail className="text-yellow-500" /><a className='email text-sm  text-center' href='mailto:dileepkumarnaidu2345@gmail.com'>dileepkumarnaidu2345@gmail.com</a></span> {/* email */}
+          <span className="flex items-center justify-items-center gap-2 text-lg p-1 m-0"><FaPhoneAlt className="text-blue-500" /><a className='phone text-sm  text-center' href='+91 9492502345'>+91 9492502345</a></span>{/* phone number */}
           <span className="flex flex-row items-center justify-items-center gap-2 text-lg p-1 m-0">
-            <FaGithub className="text-black dark:text-white" /> <a className='text-sm' href="https://github.com/javid4962" target='blank'>https://github.com/javid4962</a>
+            <FaGithub className="text-black dark:text-white" /> <a className='text-sm' href="https://github.com/DILEEP-KUMAR-7" target='blank'>https://github.com/DILEEP-KUMAR-7</a>
           </span>
           <span className="flex flex-row items-center justify-items-center gap-2 text-lg p-1 m-0">
-            <TfiLinkedin className="text-blue-400 dark:text-blue-600" /> <a className='text-sm' href="https://www.linkedin.com/in/masthan-javid-vali-shaik-3a15ba248/" target='blank'>masthan-javid-vali-shaik-3a15ba248/</a></span>
-          <span className="flex flex-row items-center justify-items-center gap-2 text-lg p-1 m-0">
-            <SiLeetcode className="text-orange-500" /> <a className='text-sm' href="https://leetcode.com/javid4962" target='blank'>https://leetcode.com/javid4962</a></span>
-          <a className='flex justify-center m-3' href='../assets/MASTHAN JAVID VALI SHAIK.pdf' download={true} target="blank">
+            <TfiLinkedin className="text-blue-400 dark:text-blue-600" /> <a className='text-sm' href="https://www.linkedin.com/in/dileep-kumar-boragadda-43293822a/" target='blank'>https://www.linkedin.com/in/dileep-kumar-boragadda-43293822a/</a></span>
+          {/* <span className="flex flex-row items-center justify-items-center gap-2 text-lg p-1 m-0"> */}
+            {/* <SiLeetcode className="text-orange-500" /> <a className='text-sm' href="" target='blank'></a></span> */}
+          <a className='flex justify-center m-3' href='../assets/BORAGADDADILEEP KUMAR_portfolio.pdf' download={true} target="blank">
             <button type="button"
               data-twe-ripple-init
               data-twe-ripple-color="black"
